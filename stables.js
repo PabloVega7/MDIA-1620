a47df8ca2a6e42b4ab8c33baa852947202aa9158
PabloVega7 <jbojorquezvega@my.bcit.ca>
2024-09-26 20:14:41 -0700

    adding "loonies"

--- stables.js
+++ stables.js
@@ -13,8 +13,8 @@ console.log(horseName + horseIntroduction + horseNickName);
 const STABLE_MONTHLY_FEE = 100
 let stableSavings = (STABLE_MONTHLY_FEE*0.1)*3;
 
-let rentIntroduction = "It costs $" + (STABLE_MONTHLY_FEE*0.9)*3 + " to board " + horseName + " for 3 months and saving $" + stableSavings + " with a 10% discount!";
-//let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName} `;
+let rentIntroduction = "It costs $" + (STABLE_MONTHLY_FEE*0.9)*3 + " loonies to board " + horseName + " for 3 months and saving $" + stableSavings + " with a 10% discount!";
+//let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} loonies to board ${horseName} `;
 console.log(rentIntroduction);
 
 //let stableIntroduction = "welcome to " + horseName + "'s stable";
