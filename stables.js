ba326a0094bedb99c619c75b8435efd463954767
PabloVega7 <jbojorquezvega@my.bcit.ca>
2024-09-26 19:18:17 -0700

    LAB 4horse

--- stables.js
+++ stables.js
@@ -13,12 +13,20 @@ let horseIntroduction = " is the name of my horse";
 
 // let rentIntroduction = "it costs " + STABLE_MONTHLY_FEE+ " to board " + horseName;
 let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`; 
+console.log (rentIntroduction);
 
 // let stableIntroduction = "Welcome to " + horseName + "'s stable";
 let stableIntroduction = `Welcome to ${horseName} 's stable` 
 console.log (stableIntroduction);
 
 //// LAB 4 CODE
+let horseNickname ="Jack D"
+let horseNicknameIntro= horseName + "is the name of y horse! and " + horseNickname "is his nickname."
+console.log(horseNicknameIntro)
+
+let discount = (STABLE_MONTHLY_FEE *3 - 3)
+let discountMessage = `Save 10 percent off 3 months which adds up to a total of ${discount}loonies`
+console.log(discountMessage)
 
 
 
