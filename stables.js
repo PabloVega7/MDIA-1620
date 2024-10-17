743a2002db2221917ed7f417c982f9612b12b961
PabloVega7 <jbojorquezvega@my.bcit.ca>
2024-10-17 12:10:14 -0700

    lesson-5

--- stables.js
+++ stables.js
@@ -1,8 +1,8 @@
 
-let horseName = "Whiskey"
-let horseNickName = "Jack"
-//let horseAge = 5;
-//let isHorseInStable = false;
+let horseName = "Whiskey";
+let horseNickName = "Jack";
+let horseAge = 5;
+let isHorseInStable = true;
 let horseIntroduction = " is the name of my horse and his nickname is ";
 
 //console.log(horseName)
@@ -10,7 +10,7 @@ let horseIntroduction = " is the name of my horse and his nickname is ";
 //console.log(isHorseInStable)
 console.log(horseName + horseIntroduction + horseNickName);
 
-const STABLE_MONTHLY_FEE = 100
+const STABLE_MONTHLY_FEE = 100;
 let stableSavings = (STABLE_MONTHLY_FEE*0.1)*3;
 
 let rentIntroduction = "It costs $" + (STABLE_MONTHLY_FEE*0.9)*3 + " loonies to board " + horseName + " for 3 months and saving $" + stableSavings + " with a 10% discount!";
@@ -20,6 +20,17 @@ console.log(rentIntroduction);
 //let stableIntroduction = "welcome to " + horseName + "'s stable";
 //let stableIntroduction = `Welcome to "${horseName}"'s stable`
 //console.log(stableIntroduction);
+
+// LESSON-5 Code
+let isHorseInside = false; 
+if(isHorseInside) {
+    console.log(horseName + " is inside!");
+} else {
+    console.log(horseName + " is outside!");
+}
+
+
+
 
 
 
